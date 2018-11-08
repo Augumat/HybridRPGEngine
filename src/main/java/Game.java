4fd22bb30eaa934e2cf346221255882bdffc32f9
Matthew Augustyn <ma62@illinois.edu>
2018-11-07 18:04:10 -0600

    Removed a bunch of garbage methods and implemented a single method that does the job of all the others. Nice.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,6 +2,7 @@ package main.java;
 
 import javax.swing.*;
 import java.awt.image.*;
+import java.awt.*;
 import java.io.*;
 import javax.imageio.*;
 
@@ -16,9 +17,14 @@ public class Game
     private static final int DEFAULT_WINDOW_WIDTH = 256;
     /** The starting title of the game window. */
     private static final int DEFAULT_WINDOW_HEIGHT = 192;
+    /** The width of the monitor that the game is playing on. */
+    private static final int FULLSCREEN_WIDTH = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    /** The height of the monitor that the game is playing on. */
+    private static int FULLSCREEN_HEIGHT = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    /** The fullscreen constant for use in resizeWindow. */
+    private static final int FULLSCREEN = 0;
     /** The starting icon of the game window. */
-    private static final BufferedImage DEFAULT_WINDOW_ICON =
-            new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+    private static final BufferedImage DEFAULT_WINDOW_ICON = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
     
     /** The width of the window as set at startup or during runtime. */
     private static int windowWidth;
@@ -30,45 +36,44 @@ public class Game
     private static BufferedImage windowIcon;
     /** The window of the game. */
     private static JFrame gameWindow;
+    
+    
+    
     /**
-     * Sets the dimensions of the window.
-     * <p>
-     *     The parameters for this method will only ever be (//stub some set values like "small, medium, large")
-     * </p>
-     *
-     * @param newWidth The new width that windowWidth will be set to.
-     * @param newHeight The new height that windowHeight will be set to.
+     * This method resizes gameWindow according to a provided scalar.
+     * @param newScale FULLSCREEN sets the window to fullscreen, other integers set the window size to the default
+     *                 parameters times this input as a scalar.  NOTE- Will not set window to a size larger than
+     *                 the monitor the window is displayed on.
      */
-    private static void setWindowDim(int newWidth, int newHeight)
+    private static void resizeWindow(final int newScale)
     {
-        windowWidth = newWidth;
-        windowHeight = newHeight;
+        gameWindow.setVisible(false);
+        if (newScale == FULLSCREEN)
+        {
+            gameWindow.setUndecorated(true);
+            gameWindow.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            windowWidth = FULLSCREEN_WIDTH;
+            windowHeight = FULLSCREEN_HEIGHT;
+        }
+        else if (newScale > 0 && newScale * Math.max(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT) < Math.min(FULLSCREEN_HEIGHT, FULLSCREEN_WIDTH))
+        {
+            gameWindow.setUndecorated(false);
+            gameWindow.setExtendedState(JFrame.NORMAL);
+            windowWidth = newScale * DEFAULT_WINDOW_WIDTH;
+            windowHeight = newScale * DEFAULT_WINDOW_HEIGHT;
+            gameWindow.setSize(windowWidth, windowHeight);
+        }
+        else
+        {
+            gameWindow.setUndecorated(false);
+            gameWindow.setExtendedState(JFrame.NORMAL);
+            windowWidth = DEFAULT_WINDOW_WIDTH;
+            windowHeight = DEFAULT_WINDOW_HEIGHT;
+            gameWindow.setSize(windowWidth, windowHeight);
+        }
         //stub set content scaling
         //stub update scaled content
-        gameWindow.setSize(windowWidth, windowHeight);
-        setWindowBorders(true);
-    }
-    /**
-     * Sets the borders of the window.
-     * <p>
-     *     Passing True will add window borders, False will set the window to borderless.
-     * </p>
-     *
-     * @param isBorderless True for window borders, False for borderless.
-     */
-    private static void setWindowBorders(boolean isBorderless)
-    {
-        //stub implement method
-    }
-    /**
-     * (//stub add javadoc)
-     */
-    private static void setFullscreen()
-    {
-        //stub set content scaling
-        //stub update scaled content
-        gameWindow.setSize(windowWidth, windowHeight);
-        setWindowBorders(false);
+        gameWindow.setVisible(true);
     }
     /** Runs when the program first launches. */
     private static void initialize()
@@ -88,18 +93,13 @@ public class Game
         gameWindow = new JFrame(windowTitle);
         gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameWindow.setIconImage(windowIcon);
-        gameWindow.setVisible(true);
-        setWindowDim(windowWidth, windowHeight);
+        resizeWindow(1);
+        //setWindowDim(windowWidth, windowHeight);
     }
     /** When run, launches the game. */
     public static void main(String unused[])
     {
         initialize();
-        //stub testing
-        while (gameWindow != null)
-        {
-            System.out.println("running...");
-        }
-        System.out.println("finished.");
+        System.out.println("Finished.");
     }
 }
